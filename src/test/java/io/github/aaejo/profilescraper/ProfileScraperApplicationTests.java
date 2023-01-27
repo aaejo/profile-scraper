1f1dd2b79f338345314a5e33289a7b53e5f0c4b7
OmriHarary <omriharary@hotmail.com>
2023-01-27 13:51:05 -0500

    Set up as Spring Boot app
    - Generated with Spring Initializer
    - Included Kafka dependencies

---
+++ src/test/java/io/github/aaejo/profilescraper/ProfileScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aaejo.profilescraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
